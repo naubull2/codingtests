31d91409a84bded2b124cd010fb7dc4a71948089
naubull2 <naubull2@gmail.com>
2021-05-19 00:22:09 +0900

    [add] solved 1603

---
+++ leetcode/solved/1603_Running_Sum_of_1d_Array/NOTE.md
@@ -0,0 +1,7 @@
+# Notes on Success
++ runtime : 36 ms (faster than 83.78% of python3 submissions)
++ memory : 14.1 MB (less than 99.74% of python3 submissions)
+
++ We perform accumulation in place, so no extra space is needed. (ignoring supplementary counter variable and such)
+
+> Time : O(N) , Space : O(1)
---
+++ leetcode/solved/1603_Running_Sum_of_1d_Array/solution.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+# Copyright © 2021 naubull2 <naubull2@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+Example 3:
+
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+
+Constraints:
+
+	1 <= nums.length <= 1000
+	-10^6 <= nums[i] <= 10^6
+"""
+import pytest
+from typing import List
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+        return nums
+
+
+@pytest.mark.parametrize('nums, expected', [
+    ([1,2,3,4], [1,3,6,10]),
+    ([1,1,1,1,1], [1,2,3,4,5]),
+    ([3,1,2,10,1], [3,4,6,16,17])
+])
+def test(nums, expected):
+    assert Solution().runningSum(nums) == expected
+
+if __name__ == '__main__':
+    sys.exit(pytest.main(['-s', '-v'] + sys.argv))
