6c7d21b1824ccb773992b937c77f19881e2987a0
naubull2 <naubull2@gmail.com>
2022-05-08 19:59:28 +0900

    [add] rearrange tree

---
+++ leetcode/solved/933_Increasing_Order_Search_Tree/NOTE.md
@@ -0,0 +1,5 @@
+# Notes on Success
++ We wouldn't use new spaces but rearrange the existing treenodes.
+  but still require H(height of tree) callstacks only.
+
+> Time : O(N) , Space : O(H)
---
+++ leetcode/solved/933_Increasing_Order_Search_Tree/solution.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+# Copyright © 2021 naubull2 <naubull2@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only one right child.
+
+Example 1:
+
+Input: root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+
+Example 2:
+
+Input: root = [5,1,7]
+Output: [1,null,5,null,7]
+
+Constraints:
+
+	The number of nodes in the given tree will be in the range [1, 100].
+	0 <= Node.val <= 1000
+"""
+import pytest
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        def _in_order(node):
+            if node:
+                _in_order(node.left)
+                node.left = None
+                self.cur.right = node
+                self.cur = node
+                _in_order(node.right)
+
+    ans = self.cur = TreeNode(None)
+    _in_order(root)
+    return ans.right
+
+
+@pytest.mark.parametrize('', [
+])
+def test():
+    pass
+
+if __name__ == '__main__':
+    sys.exit(pytest.main(['-s', '-v'] + sys.argv))
