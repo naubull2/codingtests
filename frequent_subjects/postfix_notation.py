717192e9ad36d62fe97059995389468ec4e31612
naubull2 <naubull2@gmail.com>
2022-05-10 00:58:32 +0900

    [add] non-python style

--- frequent_subjects/postfix_notation.py
+++ frequent_subjects/postfix_notation.py
@@ -8,6 +8,27 @@
 # assume only arithmetic operations e.g. no sqrt, log etc
 postfix = ['2', '1', '+', '3', '*']
 
+
+def compute_postfix(notation):
+    stack = []
+    operators = '+-*/'
+    for c in postfix:
+        if c.isnumeric():
+            stack.append(int(c))
+        else:
+            arg0 = stack.pop()
+            arg1 = stack.pop()
+            op = operators.index(c)
+            if op == 0:
+                stack.append(arg0 + arg1)
+            elif op == 1:
+                stack.append(arg0 - arg1)
+            elif op == 2:
+                stack.append(arg0 * arg1)
+            elif op == 3:
+                stack.append(arg0 / arg1)
+    return stack[0]
+    
 # let's utilize python3 string formatter for ease of implemeting arithmetic operator evaluation
 def compute_postfix(notation):
     stack = []
