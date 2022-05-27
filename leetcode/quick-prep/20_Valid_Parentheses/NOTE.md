f364920f9b4acf4189ae64e66f9de182491b34bf
naubull2 <naubull2@gmail.com>
2022-05-28 02:07:03 +0900

    [add] basic stack

---
+++ leetcode/quick-prep/20_Valid_Parentheses/NOTE.md
@@ -0,0 +1,8 @@
+# Notes on Success
++ Put opners in to stack, then on closers, check stack's
+  top to see if it matches.
++ Edge cases would be 
+  - no opener at all (unopend)
+  - no closer at all (unclosed)
+
+> Time : O(N) , Space : O(N)
---
+++ leetcode/quick-prep/20_Valid_Parentheses/solution.py
@@ -0,0 +1,68 @@
+# coding: utf-8
+# Copyright © 2021 naubull2 <naubull2@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+	Open brackets must be closed by the same type of brackets.
+	Open brackets must be closed in the correct order.
+
+Example 1:
+
+Input: s = "()"
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+Output: false
+
+Constraints:
+
+	1 <= s.length <= 104
+	s consists of parentheses only '()[]{}'.
+"""
+import pytest
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:  
+        opener = '({['
+        closer = ')}]'
+        stack = []
+        for c in s:
+            if c in opener:
+                stack.append(c)
+            else:
+                if not stack:
+                    return False # No opener at all
+                
+                last = stack.pop()
+                if closer[opener.index(last)] != c: # Not a match
+                    return False
+        if stack: # Any non-closed parenthesis
+            return False
+        return True
+                    
+
+@pytest.mark.parametrize('s, output', [
+    ('()', True),
+    ('()[]{}', True),
+    ('(]', False),
+    ('(', False),
+    (']', False)
+])
+def test(s, output):
+    assert Solution().isValid(s) == output
+
+if __name__ == '__main__':
+    sys.exit(pytest.main(['-s', '-v'] + sys.argv))
