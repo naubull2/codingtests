4041f5e766eb1e32c5d182259b25b7e6b2bc12dd
naubull2 <naubull2@gmail.com>
2022-05-29 01:28:57 +0900

    [add] group using hashkey

---
+++ leetcode/quick-prep/49_Group_Anagrams/README.md
@@ -0,0 +1,39 @@
+### [49. Group Anagrams](https://leetcode.com/problems/group-anagrams)
+
+Medium
+
+Given an array of strings `` strs ``, group __the anagrams__ together. You can return the answer in __any order__.
+
+An __Anagram__ is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+__Example 1:__
+
+```Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+```
+
+__Example 2:__
+
+```Input: strs = [""]
+Output: [[""]]
+```
+
+__Example 3:__
+
+```Input: strs = ["a"]
+Output: [["a"]]
+```
+
+ 
+
+__Constraints:__
+
+*   <code>1 <= strs.length <= 10<sup>4</sup></code>
+*   `` 0 <= strs[i].length <= 100 ``
+*   `` strs[i] `` consists of lowercase English letters.
+
+| Submissions    | Accepted     | Rate   |
+| -------------- | ------------ | ------ |
+| 2,129,751 | 1,371,673 | 64.4% |
---
+++ leetcode/quick-prep/49_Group_Anagrams/solution.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+# Copyright © 2021 naubull2 <naubull2@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+Input: strs = ["a"]
+Output: [["a"]]
+
+Constraints:
+
+	1 <= strs.length <= 104
+	0 <= strs[i].length <= 100
+	strs[i] consists of lowercase English letters.
+"""
+import pytest
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        def gen_key(st):
+            return ''.join(sorted(st))
+
+        groups = dict()
+        for s in strs:
+            key = gen_key(s)
+            groups.setdefault(key, []).append(s)
+        return groups.values()
+
+
+@pytest.mark.parametrize('', [
+])
+def test():
+    pass
+
+if __name__ == '__main__':
+    sys.exit(pytest.main(['-s', '-v'] + sys.argv))
