7b3fcbb3dff9e734bb5cdda82f24af9a09c85350
naubull2 <naubull2@gmail.com>
2022-05-30 01:25:11 +0900

    [add] split, reverse join list

---
+++ leetcode/quick-prep/143_Reorder_List/README.md
@@ -0,0 +1,48 @@
+### [143. Reorder List](https://leetcode.com/problems/reorder-list)
+
+Medium
+
+You are given the head of a singly linked-list. The list can be represented as:
+
+```
+L<sub>0</sub> → L<sub>1</sub> → … → L<sub>n - 1</sub> → L<sub>n</sub>
+```
+
+_Reorder the list to be on the following form:_
+
+```
+L<sub>0</sub> → L<sub>n</sub> → L<sub>1</sub> → L<sub>n - 1</sub> → L<sub>2</sub> → L<sub>n - 2</sub> → …
+```
+
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+ 
+
+__Example 1:__
+
+<img alt="" src="https://assets.leetcode.com/uploads/2021/03/04/reorder1linked-list.jpg" style="width: 422px; height: 222px;"/>
+
+```
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+```
+
+__Example 2:__
+
+<img alt="" src="https://assets.leetcode.com/uploads/2021/03/09/reorder2-linked-list.jpg" style="width: 542px; height: 222px;"/>
+
+```
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+```
+
+ 
+
+__Constraints:__
+
+*   The number of nodes in the list is in the range <code>[1, 5 * 10<sup>4</sup>]</code>.
+*   `` 1 <= Node.val <= 1000 ``
+
+| Submissions    | Accepted     | Rate   |
+| -------------- | ------------ | ------ |
+| 1,014,481 | 490,316 | 48.3% |
---
+++ leetcode/quick-prep/143_Reorder_List/solution.py
@@ -0,0 +1,74 @@
+# coding: utf-8
+# Copyright © 2021 naubull2 <naubull2@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+Example 1:
+
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+
+Example 2:
+
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+
+Constraints:
+
+	The number of nodes in the list is in the range [1, 5 * 104].
+	1 <= Node.val <= 1000
+"""
+import pytest
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # split list
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reverse second
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            second.next, prev, second = prev, second, second.next
+
+        first = head
+        second = prev
+
+        # join two list alternating
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
+        
+
+@pytest.mark.parametrize('', [
+])
+def test():
+    pass
+
+if __name__ == '__main__':
+    sys.exit(pytest.main(['-s', '-v'] + sys.argv))
