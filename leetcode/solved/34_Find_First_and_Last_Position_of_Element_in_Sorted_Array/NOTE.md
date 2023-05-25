89788c5d010c50696467190f160449c102db1605
dan <dan.kes@kakaoenterprise.com>
2023-05-25 21:37:18 +0900

    [fix] improve readability of the solution

--- leetcode/solved/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array/NOTE.md
+++ leetcode/solved/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array/NOTE.md
@@ -2,10 +2,14 @@
 + runtime : 72 ms (faster than 99.20% of python3 submissions)
 + memory : 15.4 MB (less than 48.02% of python3 submissions)
 
-+ In place binary search down to the min-max range of the given target.
-  - We divide process into two phases.
-    1. First find if the target exists at all.
-    2. If exists, then we binary search further into left and right partition of the found target index.
-      - In the second phase of the binary search, we binary search until the last match in the given direction is found. 
++ First start with a plain binary search,
+  then modify the part where we have found the target,
+   so we further do the binary search until we find the left/right-most targets.
+
+   Essentially we are doing 2 binary searches so the complexity is still O(logN)
+
++ We can actually improve this by a small margin by sharing the first binary search 
+  and run further searching for the left / right boundaries,
+  but for the sake of readability, I would just leave it as it is.
 
 > Time : O(log N) , Space : O(1)
--- leetcode/solved/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array/solution.py
+++ leetcode/solved/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array/solution.py
@@ -32,71 +32,50 @@ import pdb
 from typing import List
 
 
-# NOTE: ordinary binary search for comparison
-def bsearch(nums, target, left, right):
-    mid = (left + right) // 2
-    if left < right and left < mid:
-        if nums[mid] == target:
-            return mid
-        if nums[mid] < target:
-            return bsearch(nums, target, mid, right)
-        else:
-            return bsearch(nums, target, left, mid)
-    else:
-        try:
-            if nums[left] == target:
-                return left
-            elif nums[right] == target:
-                return right
-        except:
-            return -1
-        return -1
-
-
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        # search middle
-        mid = self.find_bsearch(nums, target, 0, len(nums) - 1, None)
-        if mid < 0:
-            return [-1, -1]
-        right = self.find_bsearch(nums, target, mid, len(nums) - 1, direction=1)
-        left = self.find_bsearch(nums, target, 0, mid, direction=-1)
-        return [min(left, mid), max(mid, right)]
-
-    def find_bsearch(self, nums, target, left, right, direction=None):
-        """Directed binary search when direction is given as [1 or -1]
-        Set direction=None, for an ordinary binary search
         """
-        mid = (left + right) // 2
-        if left < right and left < mid:
-            if nums[mid] == target:
-                if not direction and nums[mid] == target:
-                    return mid
-                elif direction == 1:
-                    return self.find_bsearch(nums, target, mid, right, direction)
-                else:
-                    return self.find_bsearch(nums, target, left, mid, direction)
+        [5, 7, 7, 8, 8, 10], target = 8
+        [8, 8, 8, 8,... 8, 8, 8], target = 8
+        10^5
 
-            if nums[mid] < target:
-                return self.find_bsearch(nums, target, mid, right, direction)
-            else:
-                return self.find_bsearch(nums, target, left, mid, direction)
-        else:
-            if not direction:
-                try:
-                    if nums[left] == target:
-                        return left
-                    elif nums[right] == target:
-                        return right
-                except:
-                    return -1
-                return -1
-            if direction == 1 and nums[right] == target:
-                return right
-            elif nums[left] == target:
-                return left
-            else:
-                return right
+        - modified binary search?
+         - instead of looking for exact value,
+           find value who's adjacent value is not 8 (left / right)
+        
+        2 binary searches
+
+        """
+        def binary_search(nums, target, is_left):
+            #is_left : True  = target who's left adjacent value is not target
+            #          False = target who's right adjacent value is not target
+            l, r = 0, len(nums)-1
+            while l <= r:
+                mid = (l + r) // 2
+                if nums[mid] == target:
+                    if is_left:
+                        if (
+                            (mid-1 >= 0 and nums[mid-1] != target)
+                            or mid == 0
+                        ):
+                            return mid
+                        else:
+                            r = mid - 1
+                    else:
+                        if (
+                            (mid+1 < len(nums) and nums[mid+1] != target)
+                            or (mid+1) == len(nums)
+                        ):
+                            return mid
+                        else:
+                            l = mid + 1
+                elif nums[mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            return -1
+        return [binary_search(nums, target, True), binary_search(nums, target, False)]
+
 
 
 @pytest.mark.parametrize(
@@ -114,19 +93,6 @@ class Solution:
 def test(nums, target, expected):
     assert Solution().searchRange(nums, target) == expected
 
-
-@pytest.mark.parametrize(
-    "nums, target, expected",
-    [
-        ([5, 7, 7, 8, 8, 10], 8, 3),
-        ([5, 7, 7, 8, 8, 10], 6, -1),
-        ([], 0, -1),
-        ([1, 4], 4, 1),
-    ],
-)
-def test_binary_search(nums, target, expected):
-    assert bsearch(nums, target, 0, len(nums) - 1) == expected
-
-
 if __name__ == "__main__":
-    sys.exit(pytest.main(["-s", "-v"] + sys.argv))
+    Solution().searchRange([2, 2], 2)
+    #sys.exit(pytest.main(["-s", "-v"] + sys.argv))
